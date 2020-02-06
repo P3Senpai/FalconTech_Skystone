38fd2cd31e4ad8efc1b9d9661929a94260f39961
P3Senpai <petrkmilev@gmail.com>
2020-02-06 23:37:14 +0100

    Created a hold button method in the Toggle class. The class will be renamed in the future.

--- SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Toggle.java
+++ SkyStone-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Toggle.java
@@ -8,11 +8,13 @@ import java.util.ArrayList;
 
 class Toggle{
     private ArrayList<Boolean> previousState = new ArrayList<Boolean>();
-    private static int counter;
+    private ArrayList<Double> startTimeLog = new ArrayList<Double>();
+    private static int counter, holdingCounter;
 
     /* Constructor */
     public void Toggle(){
         counter = 0;
+        holdingCounter = 0;
     }
 
     // Description of the functionality of the automatic toggle
@@ -71,12 +73,37 @@ class Toggle{
         return result;
     }
 
+    // do an action if holding down a button
+    public boolean hold(boolean button, double timeOut, double currentTime){
+        double startTime;
+        // Sets and creates list
+        try{
+            startTime = startTimeLog.get(holdingCounter); // gets start time from list
+        }catch (IndexOutOfBoundsException e){ // if used properly this will only happen in the first loop of the code
+            startTimeLog.add(-1.);
+            startTime = startTimeLog.get(holdingCounter); // creates element which will store start time
+        }
+
+        if (button){
+            if (startTime > 0){ // if start time is set check if the timer ran out
+                return (timeOut >= (currentTime - startTime));
+            }else{
+                startTimeLog.remove(holdingCounter);        // set a start time in the list
+                startTimeLog.add(holdingCounter, startTime);
+            }
+        }else{
+            startTimeLog.remove(holdingCounter); // button isn't held anymore so reset start time
+            startTimeLog.add(holdingCounter, -1.);
+        }
+        holdingCounter++; // increment through list
+        return false;
+    }
+
     // this is used with the automatic toggle method
     public void reset(){
         counter = 0;
+        holdingCounter = 0;
         System.out.println("Counter has been reset");
     }
-
-    //todo add button length pressed to activate method
 }
 
